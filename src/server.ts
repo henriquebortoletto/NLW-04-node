24103b2074d06dfd4746e41f8c06170cdd48ce0b
Henrique Bortoletto <bortolettohenrique@gmail.com>
2021-02-23 21:59:44 -0300

    adding sqlite database using typeorm, running migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata'
+import './database'
+
 import express from 'express'
+import { router } from './routes'
 
 const app = express()
-
-app.get('/', (request, response) => {
-	return response.json({ message: 'Hello Word - NLW' })
-})
-
-app.post('/', (request, response) => {
-	return response.json({ message: 'Os dados foram salvos com sucesso!' })
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(8000, () => console.log('Server is running!'))
