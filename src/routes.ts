fadd3640652653af35be32c81e48017c7bd12e4b
Henrique Bortoletto <bortolettohenrique@gmail.com>
2021-02-24 21:52:26 -0300

    create respositories and route surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserController'
+import { SurveysController } from './controllers/SurveyController'
+
 const router = Router()
 
 const userController = new UserController()
+const surveysController = new SurveysController()
 
 router.post('/users', userController.create)
+router.get('/surveys', surveysController.show)
+router.post('/surveys', surveysController.create)
 
 export { router }
